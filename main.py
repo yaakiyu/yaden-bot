cddc6269785f6008fb4fe7f911956f67788c27d6
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-01-27 08:27:05 +0900

    cogs.firstの作成

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix="ya!")
+bot = commands.Bot(
+    command_prefix="ya!",
+    help_command=None
+)
+
+bot.load_extension("cogs._first")
 
 bot.run(os.getenv("TOKEN"))
